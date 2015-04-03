ab8ddc56c99457033f7abf69eb39f5b8e046640f
Ilya Veygman <iveygman@gmail.com>
2015-04-03 16:29:06 -0700

    Initial commit of some files

---
+++ PAPIJumpDrive/PAPIJumpDrive/PAPIJumpDrive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP.IO;
+
+namespace PAPIJumpDrive
+{
+	// from https://github.com/BobPalmer/WarpDrive/blob/master/Source/WarpEngine/WarpEngine/ShipInfo.cs
+	public class ShipInfo
+	{
+		public Part ShipPart { get; set; }
+		public float BreakingForce { get; set; }
+		public float BreakingTorque { get; set; }
+		public float CrashTolerance { get; set; }
+		public RigidbodyConstraints Constraints { get; set; }
+	}
+
+	public class PAPIJumpModule : PartModule
+	{
+
+	}
+}
+
